cbe03cfcf319050e6063afb642a548a4bb4196a3
nick_vaulton <nvaulton@gmail.com>
2025-02-28 16:38:44 -0700

    C: completed bookbot

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
+filepath = sys.argv[1]
+from stats import char_report
+from stats import get_book_text
 from stats import get_num_words
+book_text = get_book_text(filepath)
+char_counts = char_report(book_text)
+numwords = get_num_words(book_text)
+print(f"============ BOOKBOT ============\nAnalyzing book found at {filepath}...\n----------- Word Count ----------\nFound {numwords} total words\n--------- Character Count -------")
+for char_dict in char_counts:
+    character = char_dict["char"]
+    count = char_dict["count"]
+    print(f"{character}: {count}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -8,15 +8,48 @@ def get_book_text(filepath):
         return file_contents
 
 def main():
-       contents = get_book_text("/home/nichard/projects/Bookbot/books/frankenstein.txt")
+       contents = get_book_text(filepath)
        split_words = contents.split()
        total_words = len(split_words)
        print(f"{total_words} words found in the document")
 
-def get_num_words():
-       contents = get_book_text("/home/nichard/projects/Bookbot/books/frankenstein.txt")
+def get_num_words(file_contents):
+       contents = file_contents
        split_words = contents.split()
        total_words = len(split_words)
-       print(f"{total_words} words found in the document")
+       return total_words
 
-main()
+def number_characters(text):
+    the_letters = {}
+    lowered = text.lower()
+    for letters in lowered:
+        if letters in the_letters:
+            the_letters[letters] +=1
+        else:
+            the_letters[letters] = 1
+    return the_letters
+
+def char_report(text):
+    alpha = {}
+    the_letters = {}
+    lowered = text.lower()
+    for letters in lowered:
+        if letters in the_letters:
+            the_letters[letters] +=1
+        else:
+            the_letters[letters] = 1      
+    for key, value in the_letters.items():
+        if key.isalpha() == True:
+            alpha[key] = value
+        else:
+            pass
+    def sort_on(dict):
+        return dict["count"]
+    
+    char_list = []
+    for char, count in alpha.items():
+        char_list.append({"char": char, "count": count})
+
+    char_list.sort(reverse=True, key=sort_on)
+
+    return char_list
